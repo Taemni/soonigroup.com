35df43f43b0e412e7dc8c43f97fcc4fe5720224a
Taemni <stm3764@nate.com>
2024-03-17 07:57:32 +0900

    update

--- src/pages/main.js
+++ src/pages/main.js
@@ -21,39 +21,10 @@ function Main() {
           </svg>
           <p className="pb-3 mb-0 small">
             <strong className="d-block text-gray-dark"># 3주 사이클</strong>
-            <span className="d-block">02.25(일) ~ 03.02(토) 프리주</span>
-            <span className="d-block">03.03(일) ~ 03.08(금) 몰입주</span>
-            <span className="d-block">03.09(토) ~ 03.15(금) 연습주</span>
-            <span className="d-block">03.16(토) 4회 수니콘 - Quater Final I</span>
-          </p>
-        </div>
-        <div className="d-flex text-body-secondary border-bottom pt-3">
-          <svg
-            className="bd-placeholder-img flex-shrink-0 me-2 rounded"
-            width="32"
-            height="32"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-label="Placeholder: 32x32"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false">
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#e83e8c"></rect>
-            <text x="50%" y="50%" fill="#e83e8c" dy=".3em">
-              32x32
-            </text>
-          </svg>
-          <p className="pb-3 mb-0 small">
-            <strong className="d-block text-gray-dark"># 드림콘서트</strong>
-            <span className="d-block text-decoration-line-through">02.28(수) 오후 6시 드림콘서트 예매 시작</span>
-            <span className="d-block text-decoration-line-through">03.02(토) 오후 8시 드림콘서트 무대 점검</span>
-            <span className="d-block text-decoration-line-through">03.03(일) 오후 3시 30분 드림콘서트 리허설 시작</span>
-            <span className="d-block text-decoration-line-through">03.03(일) 오후 7시 드림콘서트 시작</span>
-            <span className="d-block">
-              <a href="https://vod.afreecatv.com/player/117995835" target="_blank">
-                드림콘서트 다시 보기
-              </a>
-            </span>
+            <span className="d-block">03.17(일) ~ 03.23(토) 프리주</span>
+            <span className="d-block">03.24(일) ~ 03.29(금) 몰입주</span>
+            <span className="d-block">03.30(토) ~ 04.05(금) 연습주</span>
+            <span className="d-block">04.06(토) 수니콘 5회 - MindControl</span>
           </p>
         </div>
         <div className="d-flex text-body-secondary border-bottom pt-3">
@@ -74,12 +45,15 @@ function Main() {
           </svg>
           <p className="pb-3 mb-0 small">
             <strong className="d-block text-gray-dark"># 수니그룹 콘텐츠</strong>
-            <span className="d-block text-decoration-line-through">03.04(월) 철권 크루대전 (수니그룹vs최가네)</span>
-            <span className="d-block text-decoration-line-through">03.05(화) 스토브리그 (데스매치, 이적시장)</span>
-            <span className="d-block text-decoration-line-through">03.06(수) 기획팀 컨텐츠</span>
-            <span className="d-block text-decoration-line-through">03.09(토) 징계위원회, 결산, 서운쇼</span>
-            <span className="d-block text-decoration-line-through">03.12(화) 통합 중간점검</span>
-            <span className="d-block">03.14(목) 통합 최종점검</span>
+            <span className="d-block">03.18(월) 인턴 정직원 발표</span>
+            <span className="d-block">03.23(토) 전야제 (클럽 파티)</span>
+            <span className="d-block">03.24(일) ~ 03.25(월) 봄맞이 힐링여행</span>
+            <span className="d-block">03.26(화) 통합 오디션</span>
+            <span className="d-block">03.27(수) 스토브리그 (데스매치, 이적시장)</span>
+            <span className="d-block">03.30(토) 수니기획 컨텐츠, 티어 조정 위원회</span>
+            <span className="d-block">03.31(일) 징계위원회, 결산, 서운쇼</span>
+            <span className="d-block">04.02(화) 통합 중간점검</span>
+            <span className="d-block">04.04(목) 통합 최종점검</span>
           </p>
         </div>
         <div className="d-flex text-body-secondary border-bottom pt-3">
